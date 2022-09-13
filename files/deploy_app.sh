a1b4502592efc21154535943f4ca699cfc3d568c
nyalpellyomkar07 <101134832+nyalpellyomkar07@users.noreply.github.com>
2022-09-13 12:25:53 -0500

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own.
+  Welcome to ${PREFIX}'s app. The site is under maintenance and will be back when the time is right.
   <!-- END -->
 
   </div>
